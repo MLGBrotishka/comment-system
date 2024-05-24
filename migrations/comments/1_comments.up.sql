98620679460f0ef525c053e312fb7fbcf625f66a
MLGBrotishka <chapkinvlad@gmail.com>
2024-05-24 16:28:13 +0300

    added host

--- migrations/comments/1_comments.up.sql
+++ migrations/comments/1_comments.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS comments (
     id SERIAL PRIMARY KEY,
     parent_id INTEGER REFERENCES comments(id),
     level INTEGER NOT NULL CHECK (level >= 1),
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER,
     post_id INTEGER REFERENCES posts(id),
     text TEXT NOT NULL CHECK (length(text) <= 2000),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- migrations/posts/1_posts.up.sql
+++ migrations/posts/1_posts.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users(id),
+    user_id INTEGER,
     comments_enabled BOOLEAN NOT NULL DEFAULT TRUE,
     name TEXT NOT NULL CHECK (length(name) <= 255),
     text TEXT NOT NULL CHECK (length(text) <= 10000),
