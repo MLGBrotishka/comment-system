04954a89b3d3e2edb571c1a4fea5bd5470c34ab9
MLGBrotishka <chapkinvlad@gmail.com>
2024-05-24 16:54:57 +0300

    docker

--- internal/app/migrate.go
+++ internal/app/migrate.go
@@ -18,39 +18,36 @@ const (
 
 func Migrate(databaseURL string) {
 	databaseURL += "?sslmode=disable"
-
-	for _, migration := range []string{"/comments", "/posts"} {
-		var (
-			attempts = _defaultAttempts
-			err      error
-			m        *migrate.Migrate
-		)
-		for attempts > 0 {
-			m, err = migrate.New("file://migrations"+migration, databaseURL)
-			if err == nil {
-				break
-			}
-
-			log.Printf("Migrate: postgres is trying to connect, attempts left: %d", attempts)
-			time.Sleep(_defaultTimeout)
-			attempts--
+	var (
+		attempts = _defaultAttempts
+		err      error
+		m        *migrate.Migrate
+	)
+	for attempts > 0 {
+		m, err = migrate.New("file://migrations", databaseURL)
+		if err == nil {
+			break
 		}
 
-		if err != nil {
-			log.Fatalf("Migrate: postgres connect error: %s", err)
-		}
-
-		err = m.Up()
-		defer m.Close()
-		if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-			log.Fatalf("Migrate: up error: %s", err)
-		}
-
-		if errors.Is(err, migrate.ErrNoChange) {
-			log.Printf("Migrate: no change")
-			return
-		}
-
-		log.Printf("Migrate: up success")
+		log.Printf("Migrate: postgres is trying to connect, attempts left: %d", attempts)
+		time.Sleep(_defaultTimeout)
+		attempts--
 	}
+
+	if err != nil {
+		log.Fatalf("Migrate: postgres connect error: %s", err)
+	}
+
+	err = m.Up()
+	defer m.Close()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		log.Fatalf("Migrate: up error: %s", err)
+	}
+
+	if errors.Is(err, migrate.ErrNoChange) {
+		log.Printf("Migrate: no change")
+		return
+	}
+
+	log.Printf("Migrate: up success")
 }
--- migrations/posts/1_posts.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS posts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    comments_enabled BOOLEAN NOT NULL DEFAULT TRUE,
-    name TEXT NOT NULL CHECK (length(name) <= 255),
-    text TEXT NOT NULL CHECK (length(text) <= 10000),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
